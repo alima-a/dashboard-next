eef71503af0a3b6f8debc5f1f7b152660a3ee05b
a.troitskaia <a.troitskaia@avatrade.com>
2025-10-31 17:05:17 +0400

    Added Suspense for login page, preparing deploy

--- src/app/shared/config/theme.ts
+++ src/app/shared/config/theme.ts
@@ -2,9 +2,13 @@ import { createTheme } from '@mui/material/styles';
 export default createTheme({
   palette: {
     mode: 'light',
-    primary: { main: '#9c25eb' },
-    secondary: { main: '#4eb15e' },
-    background: { default: '#f7f7fa' },
+    primary: { main: '#7C3AED' },
+    secondary: { main: '#06B6D4' },
+    background: { default: '#F9FAFB' },
+    text: {
+      primary: '#111827',
+      secondary: '#4B5563',
+    },
   },
   shape: { borderRadius: 2 },
 });
