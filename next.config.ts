6d9753dbf103149cd0eee9067b2e96eca3a49644
a.troitskaia <a.troitskaia@avatrade.com>
2025-10-31 16:50:28 +0400

    Replace middleware by next config settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path((?!_next/|favicon\\.ico|auth(?:/.*)?|api/(?:login|logout)(?:/.*)?).*)',
+        missing: [{ type: 'cookie', key: 'auth' }],
+        destination: '/auth/login?from=/:path',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
